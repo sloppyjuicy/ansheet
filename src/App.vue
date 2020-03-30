693c752bb925fbe24cfd49daa48bd34baad034aa
rhofp <rhodfra@gmail.com>
2020-03-29 20:50:27 -0600

    Added results panel when exam finishes

--- src/App.vue
+++ src/App.vue
@@ -16,10 +16,13 @@
             <a class="nav-link" href="#">Nosotros</a>
           </li>
         </ul>
-        <form class="form-inline my-2 my-lg-0">
+        <!--form class="form-inline my-2 my-lg-0">
           <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
           <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
+        </form-->
+        <ul class="navbar-nav mt-2 mt-lg-0">
+          <li class="nav-item"><a class="nav-link" href="#">Simulador</a></li>
+        </ul>
       </div>
     </nav>
 
--- src/components/Examen.vue
+++ src/components/Examen.vue
@@ -1,29 +1,70 @@
 <template>
     <div class="container-xl py-4">
-        <div class="row pb-3">
-            <div class="col-md-12">
+        <div class="row">
+            <div class="col-md-12 m-3">
                 <h4>Examen de simulación</h4>
                 <h5>UNAM 2012 aréa 2</h5>
             </div>
         </div>
-        <form class="pb-4">
+        <form class="pb-3" v-if="!examenTerminado">
             <div class="row">
                 <div class="col">
-                <input type="text" class="form-control" placeholder="First name">
+                <input type="text" class="form-control" placeholder="Nombre" v-model="datosAlumno.nombre" >
                 </div>
                 <div class="col">
-                <input type="text" class="form-control" placeholder="Last name">
+                <input type="text" class="form-control" placeholder="Apellidos" v-model="datosAlumno.apellidos">
                 </div>
             </div>
         </form>
+        <div class="pb-3" v-else>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h5>{{datosAlumno.nombre}} {{datosAlumno.apellidos}}</h5>
+                    </div>
+                    <div class="col-md-6 text-center">
+                        <h6>Aciertos totales: 36</h6>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-2">
+                        <h6>Matemáticas</h6>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="progress">
+                            <div class="progress-bar" role="progressbar" style="width:90%;"
+                                aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                                10 /
+                                12
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-2">
+                        <h6>Español</h6>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="progress">
+                            <div class="progress-bar" role="progressbar" style="width:90%;"
+                                aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                                9 /
+                                12
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="row">
             <div class="col-md-12 multicol">
                 <ol>
                     <li v-for="i in numReactivos" :key="i">
                         <div v-for="n in numOpciones" :key="n" class="d-inline mr-1">
                             <label class="mr-1">{{String.fromCharCode(64+n)}}</label>
-                            <input type="radio" :name="'test-'+i" >
+                            <input type="radio" :name="'test-'+i" :disabled="examenTerminado">
                         </div>
+                        <label v-if="examenTerminado">&#x2714;</label>
                     </li>
                 </ol>
             </div>
@@ -36,8 +77,14 @@ export default {
     name:'Examen',
     data(){
         let numReactivos = 120;
+        let datosAlumno = {
+            nombre: 'Rodrigo',
+            apellidos : 'Francisco'
+        };
         return {
             numReactivos,
+            datosAlumno,
+            examenTerminado : false,
             numOpciones : 4,
             respuestas : new Array('').fill(numReactivos)
         }
