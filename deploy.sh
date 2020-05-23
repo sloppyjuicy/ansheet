27dacc844dfcad4bb0df98a3b688bce6204c70a7
rhofp <rhodfra@gmail.com>
2020-05-23 10:28:17 -0500

    fix little mistake un 2017 comipmes answers

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 npm run build
-cp -rf dist/* /data/rhofp.github.io/
-cd /data/rhofp.github.io/
+cp -rf dist/* /data/ludomatics.github.io/
+cd /data/ludomatics.github.io/
 git add -A
 git commit -m "ansheet deployment commit"
 git push -u origin master
