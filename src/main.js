3a5b13454da5eda63fba5ed05781c83f9db28698
rhofp <rhodfra@gmail.com>
2020-07-25 18:31:24 -0500

    ansheet deployment commit

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 import 'bootstrap'
 //import 'bootstrap/dist/css/bootstrap.min.css'
-import "izitoast/dist/css/iziToast.css";
+import 'izitoast/dist/css/iziToast.css'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -23,48 +23,56 @@ import Reporte from './components/Reporte'
 /*import examenComipems from './canswers/unam2017comipems.json';*/
 /*import examenComipems from './canswers/unam2019comipems.json';*/
 /*import examenComipems from './canswers/unamNAcomipems.json';*/
-import examenComipems from './canswers/comipems2018.json';
+/*import examenComipems from './canswers/comipems2018.json';*/
+/*import examenComipems from './canswers/unam2020comipems.json';*/
+// import examenComipems from './canswers/unam2009comipems.json';
+// import examenComipems from './canswers/unam2016comipems.json';
+// import examenComipems from './canswers/unam2012comipems.json'
+// import examenComipems from './canswers/COMIPEMS_simulacion.json'
+import examenComipems from './canswers/Comludo2020.json'
+
 /*import examenUniversidad from './canswers/ipn01unido.json';*/
-import examenUniversidad from './canswers/ipn01unido2.json';
+/*import examenUniversidad from './canswers/ipn01unido2.json';*/
+// import examenUniversidad from './canswers/ipn_modif2.json';
+import examenUniversidad from './canswers/ipn_modif3.json'
 
 Vue.config.productionTip = false
 
 const router = new VueRouter({
   //mode: 'history',
-  routes : [
+  routes: [
     {
-      path: '/', 
+      path: '/',
       component: Welcome,
     },
-    { 
-      path: '/comipems/:debug?', 
+    {
+      path: '/comipems/:debug?',
       component: Examen,
-      props : {examen:examenComipems}
+      props: { examen: examenComipems },
     },
-    { 
-      path: '/universidad/:debug?', 
+    {
+      path: '/universidad/:debug?',
       component: Examen,
-      props : {
+      props: {
         default: true,
-        examen:examenUniversidad
-      }
+        examen: examenUniversidad,
+      },
     },
     {
-      path:'/reporte',
+      path: '/reporte',
       component: Reporte,
-      name: 'reporte'
+      name: 'reporte',
     },
     {
-      path : '*',
-      component: NotFound
-    }
-  ]
-
+      path: '*',
+      component: NotFound,
+    },
+  ],
 })
 
 Vue.use(VueRouter)
-Vue.use(VueIziToast);
-Vue.use(firestorePlugin);
+Vue.use(VueIziToast)
+Vue.use(firestorePlugin)
 
 // Install BootstrapVue
 Vue.use(BootstrapVue)
@@ -72,6 +80,6 @@ Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
 new Vue({
-  render: h => h(App),
-  router
+  render: (h) => h(App),
+  router,
 }).$mount('#app')
