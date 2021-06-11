e26e5d401b1522294c7964e5ff838a7917e75713
rhofp <rhodfra@gmail.com>
2021-06-10 22:37:37 -0500

    Adding deployment script for github pages

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Setup variables
+localRepoPath="/f/dev/ludomatics.github.io/"
+commitMessage=${*:-"ansheet deployment"}
+
+npm run lint --fix
+npm run build
+cp -rf dist/* README.* ${localRepoPath}
+cd ${localRepoPath}
+git add -A
+git commit -m "${commitMessage}"
+git push -u origin master
+cd -
