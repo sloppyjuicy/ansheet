c52342b3442b1b84d60fbee651f3fda55f438742
rhofp <rhodfra@gmail.com>
2021-06-11 18:59:06 -0500

    Deleted archived version

--- archived/horedi/TODO.md
+++
@@ -1,6 +0,0 @@
-# HOREDI
-
-## TO DO
-
-* Corregir númeración de examenes en la sección de *historial* 
-* Al crearse un grupo se crean exámenes de UNAM, IPN y UAM solo que deshabilitados.
--- archived/horedi/public/js/alumno/selected-curso.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $("#cursoselected").change(function(){
-        var selectOption = $("#cursoselected option:selected").val();
-
-        console.log(selectOption)
-    });
-});
